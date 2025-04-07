d322c364ba32bf180d575e225e54c26d1952569d
Ronaldo Porto <portofilhoronaldo@gmail.com>
2025-04-06 23:07:12 -0300

    adicionar a classe Data

--- Java/Conta do Banco/Banco/App.java
+++ Java/Conta do Banco/Banco/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main (String[] args){
-        
+        ContaBancaria conta1 = new ContaBancaria("ROnaldo");
+        Banco.quantasContas();
+        ContaBancaria conta2 = new ContaBancaria("amigo");
+        conta1.setSaldo(1000);
+        conta2.setSaldo(400);
+        conta1.exibirInformacoes();
+        conta2.exibirInformacoes();
+
+        Banco.quantasContas();
+
+        conta1.depositar(400, "04/05/2024");
+        conta1.exibirInformacoes();
+
+        conta1.depositar(100,"60/01/2008");
+
+        conta2.sacar(200, "03/06/1995");
     }
 }
--- Java/Conta do Banco/Banco/ContaBancaria.java
+++ Java/Conta do Banco/Banco/ContaBancaria.java
@@ -2,10 +2,19 @@
 public class ContaBancaria{
     private double saldo;
     private String titular;
+    private Data dataAbertura;
+    private Data[] depositos = new Data[10];
+    private Data[] saques = new Data[10];
+    private Data[] transferencias = new Data[10];
+    private static int numDepositos = 0;
+    private static int numSaques = 0;
+    private static int numTransferecias = 0;
 
     public ContaBancaria (String nome){
         titular = nome;
         saldo = 0;
+        Banco.incrementarConta();
+        dataAbertura = new Data();
     }
 
     public double getSaldo(){
@@ -16,49 +25,102 @@ public class ContaBancaria{
         this.saldo = saldo;
     }
 
-    public void ExibirInformacoes(){
+    private void incrementaDeposito(){
+        numDepositos++;
+    }
+
+    private void incrementaSaque(){
+        numSaques++;
+    }
+
+    private void incrementaTransferencia(){
+        numTransferecias++;
+    }
+
+    public void exibirInformacoes(){
         System.out.println("Titular: "+titular);
         System.out.println("Saldo atual: " + saldo);
+        System.out.println("Data de abertura da conta: ");
+        dataAbertura.mostrar();
     }
 
-    public void depositar(double valor){
-        if (valor < 0){
-            System.out.println("Valor negativo, não é possivel realizar o deposito.\n\n");
-            return;
-        }
-        System.out.println("Valor depositado com sucesso.");
-        saldo += valor;
-    }
-
-    public void sacar (double valor){
-        if (saldo < valor){
-            System.out.println("Saldo insuficiente.\n\n");
-            return;
-        }
-        else if(valor <= 0){
-            System.out.println("Valor negativo ou nulo, nao é possivel realizar o saque.\n\n");
+    public void depositar(double valor, String data){
+        Data datinha = new Data(data);
+        Boolean aux = datinha.dataValida(datinha);
+        if (aux == false){
+            System.out.println("Data invalida, operacao cancelada.");
             return;
         }
         else{
-            System.out.println("Valor sacado com sucesso.\n\n");
-            saldo -=valor;
+            if (valor < 0){
+                System.out.println("Valor negativo, não é possivel realizar o deposito.\n\n");
+                return;
+            }
+            System.out.println("Valor depositado com sucesso.");
+            depositos[numDepositos] = datinha;
+            incrementaDeposito();
+            System.out.println("Feito no dia: ");
+            depositos[numDepositos-1].mostrar();
+            
+            saldo += valor;
         }
     }
 
-    public void tranferir(double valor, ContaBancaria conta2){
-        if (saldo >= valor && valor > 0){
-            sacar(valor);
-            conta2.depositar(valor);
-            System.out.println("Transferencia realizada com sucesso.\n\n");
-            return;
-        }
-        else if (saldo < valor){
-            System.out.println("Saldo insuficiente, nao foi possivel realizar a transferencia.\n\n");
+    public void sacar (double valor, String data){
+        Data datinha = new Data(data);
+        Boolean aux = datinha.dataValida(datinha);
+        if (aux == false){
+            System.out.println("Data invalida, operacao cancelada.");
             return;
         }
         else{
-            System.out.println("Valor negativo ou nulo, nao foi possivel realizar a transferencia.\n\n");
+            if (saldo < valor){
+                System.out.println("Saldo insuficiente.\n\n");
+                return;
+            }
+            else if(valor <= 0){
+                System.out.println("Valor negativo ou nulo, nao é possivel realizar o saque.\n\n");
+                return;
+            }
+            else{
+                System.out.println("Valor sacado com sucesso.");
+                saques[numSaques] = datinha;
+                incrementaSaque();
+                System.out.println("Feito no dia: ");
+                saques[numSaques - 1].mostrar();
+    
+                saldo -=valor;
+            }
+        }
+    }
+
+    public void tranferir(double valor, String data, ContaBancaria conta2){
+        Data datinha = new Data(data);
+        Boolean aux = datinha.dataValida(datinha);
+        if (aux == false){
+            System.out.println("Data invalida, operacao cancelada.");
             return;
+        }
+        else{
+            if (saldo >= valor && valor > 0){
+                sacar(valor, data);
+                conta2.depositar(valor, data);
+                System.out.println("Transferencia realizada com sucesso.\n\n");
+                transferencias[numTransferecias] = datinha;
+                incrementaTransferencia();
+                System.out.println("Transferencia feita no dia: ");
+                transferencias[numTransferecias - 1].mostrar();
+    
+                return;
+            }
+            else if (saldo < valor){
+                System.out.println("Saldo insuficiente, nao foi possivel realizar a transferencia.\n\n");
+                return;
+            }
+            else{
+                System.out.println("Valor negativo ou nulo, nao foi possivel realizar a transferencia.\n\n");
+                return;
+            }
         }
     }
 }
